60596b99946fc44f5c3748f641e815785f1ae7bb
Ryan FT <ryanftyan@gmail.com>
2023-08-04 22:19:17 +0930

    Added new test cases

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -8,6 +8,23 @@ int main(void){
         {9,10}
     };
     int sum = sum_diagonal(arr);
-    printf("%d",sum);
+    printf("%d\n",sum);
+
+    int arr2[4][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12},
+        {13,14,15,16}
+    };
+    int sum2 = sum_diagonal(arr2);
+    printf("%d\n",sum);
+
+    int arr3[4][4] = {
+        {1,2,3,4},
+        {5,6,7,8},
+        {9,10,11,12}
+    };
+    int sum3 = sum_diagonal(arr3);
+    printf("%d\n",sum);
 
 }
