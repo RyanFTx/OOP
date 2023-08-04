eb30113d663652e4903d351dda09ae4483a92f0e
Ryan FT <ryanftyan@gmail.com>
2023-08-04 22:01:17 +0930

    Fixed

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -9,15 +9,5 @@ int sum_diagonal(int array[4][4]){
 }
 
 
-/*
-int main(void){
-    int arr[4][4] = {
-        {1,2,3,4},
-        {5,6,7,8},
-        {9,10,11,12}
-    };
-    int sum = sum_diagonal(arr);
-    printf("%d",sum);
 
-}
-*/
+ 
